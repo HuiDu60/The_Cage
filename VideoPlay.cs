84e10f4a3d21e7951ad8e2ed0dba55bb26ecd740
Hui Du <huidu60@gmail.com>
2018-03-05 14:55:50 -0500

    Add scripts

---
+++ VideoPlay.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlay : MonoBehaviour {
+
+    public VideoClip[] VideoClips;
+
+    public int VideoClipIndex;
+
+    private VideoPlayer VideoPlayer;
+
+    private void Awake()
+    {
+        VideoPlayer = GetComponent<VideoPlayer>();
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+        //VideoClipIndex = VideoClipIndex % VideoClips.Length; 
+        VideoClipIndex = 0;
+        VideoPlayer.clip = VideoClips[VideoClipIndex];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
